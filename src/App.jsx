bdba9e340c18a535c89f77fd7ffbc5cf3f4245de
Domingos chiconela <azevedodomingos78@gmail.com>
2024-08-30 21:02:31 +0200

    update:created mechanism to show the user's exact location on the map

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 import './App.css'
 import { MapContainer,Marker,TileLayer,Popup} from "react-leaflet"
 import "leaflet/dist/leaflet.css";
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [userLocation, setUserLocation] = useState(null);
+
+  useEffect(() => {
+    
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation([latitude, longitude]); 
+      },
+      (error) => {
+        console.error('Erro ao obter localização:', error);
+      }
+    );
+  }, []);
+
+  console.log(userLocation);
+  
 
   return (
-
     <div className='p-5 bg-zinc-800 flex justify-center w-fit h-fit m-auto'>
-    <div  className='leaflet-container'>
-
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-        
-          // url="http://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-
-    </div></div>
-    
-    
-  )
+      <div className='leaflet-container'>
+        {userLocation ? (
+          <MapContainer center={userLocation} zoom={13} scrollWheelZoom={true} style={{ height: '100vh', width: '100vw' }}>
+            <TileLayer
+              url="http://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}"
+            />
+            <Marker position={userLocation}>
+              <Popup>
+                Sua localização
+              </Popup>
+            </Marker>
+          </MapContainer>
+        ) : (
+          <p>Obtendo localização...</p> 
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
